dc838c68f3e74a6ccb83e7975feff47c483e33cb
Nguyen An <nguyenan21062005ne@gmail.com>
2025-05-10 02:11:37 +0700

    Public quyền cho khách hàng

--- backend/src/categories/category.controller.ts
+++ backend/src/categories/category.controller.ts
@@ -7,48 +7,41 @@ import { PermissionsGuard } from "src/guards/roles.guard";
 import { Roles } from "src/decorators/roles.decorator";
 
 @Controller("categories")
-@UseGuards(JwtAuthGuard, PermissionsGuard)
 export class CategoryController {
 
     constructor(private categoryService: CategoryService){}
 
     @Get()
-    @Roles('monan', 'donhang')
     async findAll(): Promise<CategoryResponseDto[]>{
         const categories = await this.categoryService.findAll();
         return categories;
     }
 
     @Get(":id")
-    @Roles('monan', 'donhang')
     async findOne(@Param('id') id: number): Promise<CategoryResponseDto>{
         const category = await this.categoryService.findOne(id);
         return category;
     }
 
     @Get(":id/products")
-    @Roles('monan', 'donhang')
     async getProducts(@Param('id') id: number){
         const products = this.categoryService.getProductsByCategoryID(id);
         return products;
     }
 
     @Post()
-    @Roles('monan', 'donhang')
     async create(@Body() createCategoryDto: CreateCategoryDto): Promise<CategoryResponseDto>{
         const category = await this.categoryService.create(createCategoryDto);
         return category;
     }
 
     @Put(":id")
-    @Roles('monan', 'donhang')
     async update(@Param("id") id: number, @Body() updateCategoryDto): Promise<CategoryResponseDto>{
         const category = await this.categoryService.update(id, updateCategoryDto);
         return category;
     }
 
     @Delete(":id")
-    @Roles('monan', 'donhang')
     async delete(@Param("id") id: number){
         this.categoryService.delete(id)
     }
--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -8,52 +8,44 @@ import { PermissionsGuard } from 'src/guards/roles.guard';
 import { Roles } from 'src/decorators/roles.decorator';
 
 @Controller('products')
-@UseGuards(JwtAuthGuard, PermissionsGuard)
 export class ProductsController {
     constructor(private readonly productsService: ProductsService) {}
 
     @Get()
-    @Roles('monan', 'donhang')
     async findAll(): Promise<ProductResponseDto[]> {
         const products = await this.productsService.findAll();
         return products;
     }
 
     @Get(':id')
-    @Roles('monan', 'donhang')
     async findOne(@Param('id') id: number): Promise<ProductResponseDto> {
         const product = await this.productsService.findOne(id)  //đã validate
         return product;
     }
 
     @Post()
-    @Roles('monan', 'donhang')
     async create(@Body() createProductDto: CreateProductDto): Promise<ProductResponseDto> {
         const product = await this.productsService.create(createProductDto);
         return product;
     }
 
     @Put(':id')
-    @Roles('monan', 'donhang')
     async update(@Param('id') id: number, updatedProductDto: UpdateProductDto): Promise<ProductResponseDto>{
         const product = await this.productsService.update(id, updatedProductDto);
         return product;
     }
 
     @Put(':id/lock')
-    @Roles('monan', 'donhang')
     async lock(@Param('id') id: number): Promise<void>{
         const product = await this.productsService.lock(id);
     }
 
     @Put(':id/unlock')
-    @Roles('monan', 'donhang')
     async unLock(@Param('id') id: number): Promise<void>{
         const product = await this.productsService.unLock(id);
     }
 
     @Delete(':id')
-    @Roles('monan', 'donhang')
     async delete(@Param('id') id: number): Promise<void>{
         const product = await this.productsService.delete(id);
     }
